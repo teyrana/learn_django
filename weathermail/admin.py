5de15e60ca495c21998922f0e76c1847c9274381
Daniel_M_Williams <equipoise@gmail.com>
2016-11-26 22:46:13 -0500

    [add] added recipient model & migration

--- weathermail/admin.py
+++ weathermail/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import City
+from .models import City, Recipient
 
 admin.site.register(City)
+admin.site.register(Recipient)
--- weathermail/models.py
+++ weathermail/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
-
 
 class City(models.Model):
     display_name = models.CharField(max_length=50)
     api_name = models.CharField(max_length=50)
-    population = models.IntegerField(default=0);
+    population = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.display_name)+" pop: "+str(self.population)+" ("+str(self.api_name)+")";
+
+class Recipient( models.Model ):
+    email = models.CharField(max_length=160, unique=True, blank=False)
+    location = models.ForeignKey( City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.email)+"     receiving weather for: "+str(self.location);
